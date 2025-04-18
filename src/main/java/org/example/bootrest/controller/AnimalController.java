1780336b2c4a2b98d0258dd5b98fa47ce6de8d6d
qus0in <qus0in@gmail.com>
2025-04-18 12:36:00 +0900

    find all

--- src/main/java/org/example/bootrest/controller/AnimalController.java
+++ src/main/java/org/example/bootrest/controller/AnimalController.java
@@ -1,7 +1,28 @@
 package org.example.bootrest.controller;
 
+import org.example.bootrest.model.domain.Animal;
+import org.example.bootrest.service.AnimalService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/animal")
+import java.util.List;
+
+@RestController // Spring -> 이거 아니라고... 주소 아니고
+@RequestMapping("/api/animal") // 이겁니다...
 public class AnimalController {
+    private final AnimalService animalService;
+
+    public AnimalController(AnimalService animalService) {
+        this.animalService = animalService;
+    }
+
+    //    @GetMapping("/hello")
+//    public String hello() {
+//        return "hello";
+//    }
+    @GetMapping
+    public List<Animal> all() {
+        return animalService.findAll();
+    }
 }
